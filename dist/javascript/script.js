b18f8da19e2f11eeebbb653ad22587ced44705a6
danielnoo <dannynoo@gmail.com>
2020-12-17 14:43:39 -0500

    working on team-switching

--- dist/javascript/script.js
+++ dist/javascript/script.js
@@ -4,19 +4,33 @@ const messageContainer = document.getElementById('message-container')
 const messageInput = document.getElementById('message-input')
 import Deck from './deck.js'
 
+const goodTeam = document.querySelector('#good')
+const evilTeam = document.querySelector('#evil')
+let players = {}
 
 const userName = prompt('What is your name?')
 appendMessage('You joined')
 socket.emit('new-user', userName)
-console.log(socket)
+
 socket.on('chat-message', data => {
   appendMessage(`${data.userName}: ${data.message}`)
+})
+
+// update player list and team they're on 
+socket.on('player-list', userList => {
+    players = userList;
+    console.log(players)
 })
 
 socket.on('user-connected', userName => {
   appendMessage(`${userName} connected`)
 })
-
+socket.on('join-good', user => {
+  let li = document.createElement("li")
+  li.id = user
+  li.innerHTML = `${user}`
+  goodTeam.appendChild(li)
+})
 socket.on('user-disconnected', userName => {
   appendMessage(`${userName} disconnected`)
 })
@@ -36,17 +50,19 @@ messageForm.addEventListener('submit', e => {
 
 /// card game
 
-const deck = new Deck()
 
-console.log(deck)
 
 const shuffleButton = document.querySelector('#shuffle')
 shuffleButton.addEventListener('click', () => {
-  deck.shuffle()
-  console.log(deck)
+   socket.emit('shuffle')
 })
-
-
+console.log(players[socket.id])
+const switchTeamsButton = document.querySelector('#switchTeams')
+switchTeamsButton.addEventListener('click', () => {
+  if(players['team'] === "Good"){
+  goodTeam.removeChild(document.getElementById(`${userName}`))
+  }
+})
 
 
 
--- index.js
+++ index.js
@@ -10,8 +10,14 @@ app.use('/', express.static(path.join(__dirname, 'dist')));
 
 io.on('connection', socket => {
   socket.on('new-user', userName => {
-    users[socket.id] = userName
+    users[socket.id] = {
+      'name': userName,
+      'team': 'Good',
+      'cards': []
+    }
     socket.broadcast.emit('user-connected', userName)
+    socket.emit('player-list', users)
+    socket.emit('join-good', users[socket.id]['name'])
   })
   socket.on('send-chat-message', message => {
     socket.broadcast.emit('chat-message', { message: message, userName: users[socket.id] })
@@ -19,8 +25,13 @@ io.on('connection', socket => {
   socket.on('disconnect', () => {
     socket.broadcast.emit('user-disconnected', users[socket.id])
     delete users[socket.id]
+    socket.emit('player-list', users)
   })
-  
+  socket.on('shuffle', () => {
+    deck.shuffle()
+    console.log(deck)
+    console.log(users[socket.id]['name'])
+  })
 })
 
 app.get('/', (req, res) => {
@@ -32,9 +43,7 @@ app.get('/', (req, res) => {
 
 
 
-const deck = new Deck()
-deck.shuffle()
-console.log(deck)
+
 
 
 
@@ -43,4 +52,6 @@ http.listen(5500, () => {
 });
 
 /// moving deck stuff to server side
+
+const deck = new Deck()
 

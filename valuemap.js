83736b27c27696bdcb59e0c39f9cae8b669c5b74
danielnoo <dannynoo@gmail.com>
2020-12-20 23:01:59 -0500

    started valuemap

---
+++ valuemap.js
@@ -0,0 +1,48 @@
+// put all objects in an array, then add logic at the bottom
+
+function valueMap(lead, trump) {
+  let heartValues = {
+    "9": 1,
+    "10": 2,
+    J: 3,
+    Q: 4,
+    K: 5,
+    A: 6
+  }
+  let diamondValues = {
+    "9": 1,
+    "10": 2,
+    J: 3,
+    Q: 4,
+    K: 5,
+    A: 6
+  }
+  let clubValues = {
+    "9": 1,
+    "10": 2,
+    J: 3,
+    Q: 4,
+    K: 5,
+    A: 6
+  }
+  let spadeValues = {
+    "9": 1,
+    "10": 2,
+    J: 3,
+    Q: 4,
+    K: 5,
+    A: 6
+  }
+  let trumpValues = {
+    "9": 7,
+    "10": 8,
+    J: 13,
+    Q: 9,
+    K: 10,
+    A: 11
+  }
+
+
+
+
+}

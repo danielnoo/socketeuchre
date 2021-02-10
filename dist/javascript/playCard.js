0a5400689c47b8d124b72b638ffcad6cde5b80ae
danielnoo <dannynoo@gmail.com>
2021-02-09 23:10:32 -0500

    taking new approach to touch compatibility

--- dist/javascript/playCard.js
+++ dist/javascript/playCard.js
@@ -195,3 +195,33 @@ function followSuit(gameStats){
 
 }
 
+// a function I borrowed from stack overflow that helps touch events mimick click events
+
+function touchHandler(event) {
+  var touch = event.changedTouches[0];
+
+  var simulatedEvent = document.createEvent("MouseEvent");
+      simulatedEvent.initMouseEvent({
+      touchstart: "mousedown",
+      touchmove: "mousemove",
+      touchend: "mouseup"
+  }[event.type], true, true, window, 1,
+      touch.screenX, touch.screenY,
+      touch.clientX, touch.clientY, false,
+      false, false, false, 0, null);
+
+  touch.target.dispatchEvent(simulatedEvent);
+  event.preventDefault();
+}
+
+// try this first -- or maybe try just adding these and removing them later
+
+function initTouchMimic() {
+  document.addEventListener("touchstart", touchHandler, true);
+  document.addEventListener("touchmove", touchHandler, true);
+  document.addEventListener("touchend", touchHandler, true);
+  document.addEventListener("touchcancel", touchHandler, true);
+}
+
+
+

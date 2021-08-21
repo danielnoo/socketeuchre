dcc3d31c4dff5c74bfdca5585e1171f22a20f9df
danielnoo <dannynoo@gmail.com>
2021-08-21 10:47:36 -0400

    refactoring room js, left comments - changed setName to an input from a button - remove alert method of setting name

--- dist/javascript/rooms.js
+++ dist/javascript/rooms.js
@@ -2,6 +2,9 @@ import { socket } from './script.js';
 
 
 
+// refactor to remove the alert - make it so that setting a name will enable the start game and/or join room buttons
+// restrict length 
+// allow alphanumeric only
 
 export function setNameAlert() {
   const setName = document.querySelector('#setName')
